aaff36fa4b4d50d9a0a3af85ec9e1b12328bac98
Laura López <lauralopez166@gmail.com>
2023-11-22 08:37:43 +0100

    desglose

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+const balls = [];
+
+while (balls.length < 25) { //CAMBIO
+    const size = random(10, 20);
+    const ball = new Ball(
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7, 7),
+        random(-7, 7),
+        size
+    );
+
+    balls.push(ball);
+}
+
+
+
+loop();
---
+++ scripts/canvas.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerHeight;
+const height = canvas.height = window.innerHeight;
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+function loop() {
+    ctx.fillStyle = 'rgba(0, 245, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+        for (const otherBall of balls) {
+            if (ball !== otherBall) {
+                ball.collisionDetect(otherBall);
+            }
+        }
+    }
+
+    requestAnimationFrame(loop);
+}
